be957b539522ffad46d211714be052d846543b1e
FranciscoMateusVG <49985463+FranciscoMateusVG@users.noreply.github.com>
2023-05-06 15:12:34 -0300

    refactors

--- src/js/script2.js
+++ src/js/script2.js
@@ -1,16 +1,18 @@
 //Efeito menu
-window.addEventListener('scroll', function(){
-    var menu = document.querySelector('.menu');
-    menu.classList.toggle('sticky', window.scrollY > 0);
+window.addEventListener('scroll', function () {
+  var menu = document.querySelector('.menu')
+  if (menu) {
+    menu.classList.toggle('sticky', window.scrollY > 0)
+  }
 })
 
 //Envio de dados formulario
 function enviarEmail() {
-    var nome = document.querySelector("#nome").value;
-    var email = document.querySelector("#email").value;
-    var telefone = document.querySelector("#telefone").value;
-    var mensagem = document.querySelector("#mensagem").value;
-    var body = `
+  var nome = document.querySelector('#nome').value
+  var email = document.querySelector('#email').value
+  var telefone = document.querySelector('#telefone').value
+  var mensagem = document.querySelector('#mensagem').value
+  var body = `
     Nome
     ${nome}
 
@@ -23,60 +25,58 @@ function enviarEmail() {
     E-mail
     ${email}
     `
-    var link = "mailto:contato@quebecengenharia.com.br"
-             + "?subject=" + encodeURIComponent("Contato site")
-             + "&body=" + encodeURIComponent(body)
-    ;
-    
-    window.location.href = link;
-    return
+  var link =
+    'mailto:contato@quebecengenharia.com.br' +
+    '?subject=' +
+    encodeURIComponent('Contato site') +
+    '&body=' +
+    encodeURIComponent(body)
+  window.location.href = link
+  return
 }
 
 //Animacao de conteudo
 const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        console.log(entry)
-        if (entry.isIntersecting) {
-            entry.target.classList.add('showleft');
-        }
-
-    });
-});
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting) {
+      entry.target.classList.add('showleft')
+    }
+  })
+})
 
 const hiddenElements = document.querySelectorAll('.hiddenleft')
-hiddenElements.forEach((el) => observer.observe(el));
+hiddenElements.forEach((el) => observer.observe(el))
 
 const observer2 = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        console.log(entry)
-        if (entry.isIntersecting) {
-            entry.target.classList.add('showup');
-        }
-
-    });
-});
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting) {
+      entry.target.classList.add('showup')
+    }
+  })
+})
 
 const hiddenElements2 = document.querySelectorAll('.hiddenup')
-hiddenElements2.forEach((el) => observer2.observe(el));
+hiddenElements2.forEach((el) => observer2.observe(el))
 
 const observer3 = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        console.log(entry)
-        if (entry.isIntersecting) {
-            entry.target.classList.add('showright');
-        }
-
-    });
-});
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting) {
+      entry.target.classList.add('showright')
+    }
+  })
+})
 
 const hiddenElements3 = document.querySelectorAll('.hiddenright')
-hiddenElements3.forEach((el) => observer3.observe(el));
+hiddenElements3.forEach((el) => observer3.observe(el))
 
 //menu hamburger
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
+const hamburger = document.querySelector('.hamburger')
+const navMenu = document.querySelector('.nav-menu')
 
-hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active')
+  navMenu.classList.toggle('active')
 })
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -49,10 +49,90 @@ module.exports = {
       minify: false
     }),
     new HtmlWebpackPlugin({
-      filename: 'barragem.html',
+      filename: 'Barragem.html',
       chunks: ['globals', 'script2'],
       title: 'Quebec Engenharia',
       template: './src/paginas/Barragem/Barragem.html',
+      description: 'Quebec Engenharia',
+      minify: false
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'Clientes.html',
+      chunks: ['globals', 'script2'],
+      title: 'Quebec Engenharia',
+      template: './src/paginas/Clientes/Clientes.html',
+      description: 'Quebec Engenharia',
+      minify: false
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'Contato.html',
+      chunks: ['globals', 'script2'],
+      title: 'Quebec Engenharia',
+      template: './src/paginas/Contato/Contato.html',
+      description: 'Quebec Engenharia',
+      minify: false
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'Eolica.html',
+      chunks: ['globals', 'script2'],
+      title: 'Quebec Engenharia',
+      template: './src/paginas/Eolica/Eolica.html',
+      description: 'Quebec Engenharia',
+      minify: false
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'Esg.html',
+      chunks: ['globals', 'script2'],
+      title: 'Quebec Engenharia',
+      template: './src/paginas/Esg/Esg.html',
+      description: 'Quebec Engenharia',
+      minify: false
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'Fotovoltaica.html',
+      chunks: ['globals', 'script2'],
+      title: 'Quebec Engenharia',
+      template: './src/paginas/Fotovoltaica/Fotovoltaica.html',
+      description: 'Quebec Engenharia',
+      minify: false
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'Hidreletrica.html',
+      chunks: ['globals', 'script2'],
+      title: 'Quebec Engenharia',
+      template: './src/paginas/Hidreletrica/Hidreletrica.html',
+      description: 'Quebec Engenharia',
+      minify: false
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'Industrial.html',
+      chunks: ['globals', 'script2'],
+      title: 'Quebec Engenharia',
+      template: './src/paginas/Industrial/Industrial.html',
+      description: 'Quebec Engenharia',
+      minify: false
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'Linhas.html',
+      chunks: ['globals', 'script2'],
+      title: 'Quebec Engenharia',
+      template: './src/paginas/Linhas/Linhas.html',
+      description: 'Quebec Engenharia',
+      minify: false
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'Obras.html',
+      chunks: ['globals', 'script2'],
+      title: 'Quebec Engenharia',
+      template: './src/paginas/Obras/Obras.html',
+      description: 'Quebec Engenharia',
+      minify: false
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'Sobre.html',
+      chunks: ['globals', 'script2'],
+      title: 'Quebec Engenharia',
+      template: './src/paginas/Sobre/Sobre.html',
       description: 'Quebec Engenharia',
       minify: false
     })
